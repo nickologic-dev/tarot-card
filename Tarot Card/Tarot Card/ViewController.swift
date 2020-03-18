e9934b8be961b40478fd06955387532e281affb7
nbeilis <nicole.beilis@gmail.com>
2020-03-18 12:52:02 -0400

    view cleaning

--- Tarot Card/Tarot Card/ViewController.swift
+++ Tarot Card/Tarot Card/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let APP_NAME = "My Free Tarot"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.titleLabel.text = APP_NAME
         // Do any additional setup after loading the view.
     }
 
+    @IBOutlet weak var refreshOutlet: UIButton!
+    
     @IBOutlet weak var defaultDeckImage: UIImageView!
     
     @IBOutlet weak var enterQuestion: UITextField!
     
     @IBAction func refresh(_ sender: UIButton) {
+        self.refreshOutlet.tintColor = UIColor.black //this turned it black on click
     }
     @IBAction func beginReading(_ sender: UIButton) {
     }
