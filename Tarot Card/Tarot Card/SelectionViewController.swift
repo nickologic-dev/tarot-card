73c08f77837b80882ef727fed7c2e0cd9cf3c49f
nbeilis <nicole.beilis@gmail.com>
2020-03-19 14:25:47 -0400

    add button functionality

---
+++ Tarot Card/Tarot Card/SelectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SelectionViewController.swift
+//  Tarot Card
+//
+//  Created by Nicole Beilis on 3/19/20.
+//  Copyright © 2020 nickelogic. All rights reserved.
+//
+
+import UIKit
+
+class SelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        self.navigationController?.setNavigationBarHidden(false, animated: true)
+        self.navigationController?.navigationBar.tintColor = UIColor.black;
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for:.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.layoutIfNeeded()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Tarot Card/Tarot Card/ViewController.swift
+++ Tarot Card/Tarot Card/ViewController.swift
@@ -14,11 +14,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
         self.titleLabel.text = APP_NAME
         self.startButtonOutlet.layer.cornerRadius = 25.0
+        self.enterQuestion.text = ""
+        self.refreshOutlet.tintColor = UIColor.gray
+        self.refreshOutlet.isEnabled = false
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func canRefresh(_ sender: UITextField) {
+        if (self.enterQuestion.text != "") {
+            self.refreshOutlet.tintColor = UIColor.black
+            self.refreshOutlet.isEnabled = true
+        }
+        else {
+            self.refreshOutlet.tintColor = UIColor.gray
+            self.refreshOutlet.isEnabled = false
+        }
+    }
+    
     @IBOutlet weak var startButtonOutlet: UIButton!
     
     @IBOutlet weak var refreshOutlet: UIButton!
@@ -28,13 +43,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterQuestion: UITextField!
     
     @IBAction func refresh(_ sender: UIButton) {
-        if (self.enterQuestion.text != "") {
-            self.refreshOutlet.tintColor = UIColor.black //this turned it black on click
-        }
+        self.enterQuestion.text = ""
+        self.refreshOutlet.tintColor = UIColor.gray
+        self.refreshOutlet.isEnabled = false
     }
+    
     @IBAction func beginReading(_ sender: UIButton) {
         let QUESTION = self.enterQuestion.text
     }
+    
     @IBOutlet weak var titleLabel: UILabel!
 }
 
